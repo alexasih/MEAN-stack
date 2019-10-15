f300121b017f1131b1a0d5cdfe26efcb4a8d931a
Alexa Sih <asih@bu.edu>
2019-10-14 23:23:09 -0400

    PS3

---
+++ PS3/routes/ps3.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const pug = require('pug');
+const compiledFunction = pug.compileFile('public/template.pug');
+
+router.route('/')
+    .get(function (req, res, next) {
+        //vars on query string req.query
+        res.send(compiledFunction({
+            name: 'Hey now!'
+        }));
+
+    })
+    .post(function (req, res, next) {
+        //vars on req.body
+        // res.send(`Bar has the value ${req.body.bar}`);
+        res.send(compiledFunction({
+            name: 'Text: ' + req.body.stringy,
+            lengthOfName: 'Length of text: ' + req.body.stringy.length
+        }));
+    });
+
+module.exports = router;
+
