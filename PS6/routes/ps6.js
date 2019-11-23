d6eb35ad7138ebc7ed04405a2a3f9cc0118bfa4d
Alexa Sih <asih@bu.edu>
2019-11-23 18:12:09 -0500

    fixing PS6

--- PS6/routes/ps6.js
+++ PS6/routes/ps6.js
@@ -8,51 +8,52 @@ const db = require('../mongo/mongo');
 // OLD FUNCTION
 router.route('/')
     .get(function (req, res, next) {
-        const apiKey = 'ALEXAS_API_KEY';
+        const apiKey = '3637536f8628dadf8736af7c6a1dea22';
         const city = 'boston';
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
         let mongo = db.getDB();
 
         request(url, function (err, response, body) {
-            if(err){
+            if (err) {
                 console.log('error:', err);
             } else {
                 let currentWeather = JSON.parse(body);
-                if(currentWeather.main == undefined || currentWeather.weather==undefined){
+                if (currentWeather.main == undefined || currentWeather.weather == undefined) {
                     res.send(compiledFunction({
                         weather: 'Error, please try again'
                     }));
                 } else {
-                    if (mongo.collection('weatherdata').find({'temperature': `${currentWeather.main.temp}`}).limit(1)) {
-                        mongo.collection('weatherdata').findOne({}, {'temperature': `${currentWeather.main.temp}`}, (err, result) => {
-                            if (err) throw err;
-                            console.log(result);
-                            res.send(compiledFunction({
-                                inDB: `Temperature is already in DB! fetching data....`,
-                                weather: `Today's weather is ${result.description}. Currently, the temperature is at ${result.temperature} degrees in ${result.city}!`
-                            }));
-                        });
-                    } else {
-                        mongo.collection('weatherdata').insertOne({
-                            description: `${currentWeather.weather[0].description}`,
-                            temperature: `${currentWeather.main.temp}`,
-                            city: `${currentWeather.name}`
-                        });
+                    console.log(mongo.collection('weatherdata').find({'temperature': '275.99'}));
+                    mongo.collection('weatherdata').find({'temperature': `${currentWeather.main.temp}`}).count((err, countValue) => {
+                        if (countValue > 0) {
+                            mongo.collection('weatherdata').findOne({'temperature': `${currentWeather.main.temp}`}, (err, result) => {
+                                if (err) throw err;
+                                console.log(result);
+                                res.send(compiledFunction({
+                                    inDB: `Temperature is already in DB! fetching data....`,
+                                    weather: `Today's weather is ${result.description}. Currently, the temperature is at ${result.temperature} degrees in ${result.city}!`
+                                }));
+                            });
+                        } else {
+                            mongo.collection('weatherdata').insertOne({
+                                description: `${currentWeather.weather[0].description}`,
+                                temperature: `${currentWeather.main.temp}`,
+                                city: `${currentWeather.name}`
+                            });
 
-                        mongo.collection('weatherdata').findOne({}, (err, result) => {
-                            if (err) throw err;
-                            console.log(result);
-                            res.send(compiledFunction({
-                                inDB: `Temperature not yet in DB! Adding to mongoDB....`,
-                                weather: `Today's weather is ${result.description}. Currently, the temperature is at ${result.temperature} degrees in ${result.city}!`
-                            }));
-                        });
-
-                    }
+                            mongo.collection('weatherdata').findOne({}, (err, result) => {
+                                if (err) throw err;
+                                console.log(result);
+                                res.send(compiledFunction({
+                                    inDB: `Temperature not yet in DB! Adding to mongoDB....`,
+                                    weather: `Today's weather is ${result.description}. Currently, the temperature is at ${result.temperature} degrees in ${result.city}!`
+                                }));
+                            });
+                        }
+                    })
                 }
             }
         });
-
     });
 
 db.connect((err, client) => {
